3f11a85957d449b42a5f94d46db474e4f66d6cbc
faithOnly <firdevssyildirimm@gmail.com>
2020-10-13 12:41:27 -0500

    tuple

--- Dictionaries.py
+++ Dictionaries.py
@@ -15,3 +15,33 @@ mydict["email"] = "frdevs123@gmail.com"
 print(mydict)
 mydict["email"] = "firdevs@gmail.com"
 print(mydict)
+
+#deletes the name and its value
+del mydict["name"]
+print(mydict)
+#deletes the age key value pair
+mydict.pop("age")
+print(mydict)
+# removes the last element
+mydict.popitem()
+print(mydict)
+
+if "name" in mydict1:
+    print(mydict1["name"])
+
+try:
+    print(mydict1["age"])
+except:
+    print("Error")
+
+for key in mydict1:
+    print(key)
+
+for key in mydict1.keys():
+    print(key)
+
+for value in mydict1.values():
+    print(value)
+
+for key, value in mydict1.items():
+    print(key, value)
